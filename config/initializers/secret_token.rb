476c10d593b20859c9bab6f721b4ac7eedfae375
Nicole Skoglund <n.c.skoglund@gmail.com>
2014-04-21 22:31:10 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NcsBlocipedia::Application.config.secret_key_base = '266c4034841e846bcec728a701c86e5b52d6db9a78c6387a807d7a5f4f4ea1b4c4347978d5a817097e064f4a2cd97efb8e52ec807a40b8a7073887a0cbeb53fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NcsBlocipedia::Application.config.session_store :cookie_store, key: '_ncs-blocipedia_session'
