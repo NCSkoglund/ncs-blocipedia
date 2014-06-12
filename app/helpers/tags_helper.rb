7b60e4fe6befaf7793fac263d3189fc37558df5b
Nicole Skoglund <n.c.skoglund@gmail.com>
2014-06-11 23:22:43 -0500

    Refactored tags views with helper

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,10 @@
 module TagsHelper
+
+  # for use in tags#index and tags#show
+  def is_wiki_viewable?(current_user, wiki)
+    return true if !wiki.private
+    return true if current_user && (current_user.level?(:premium) || current_user.level?(:admin))
+    return false
+  end
+
 end
